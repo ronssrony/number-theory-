7d6abd5400b2cb0f29c6bef2b0ae8681cca9e32f
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2023-02-08 05:05:31 +0600

    Create Find two unique numbers .cpp

---
+++ Find two unique numbers .cpp
@@ -0,0 +1,39 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int arr[] = {1,2,3,1,2,3,5,7} ;
+    int ans = 0 ;
+    for(int i = 0 ; i<8 ; i++)
+    {
+        ans^=arr[i] ;
+      
+    }
+    int temp = ans;
+    int setbit = 0 ;
+    int pos = 0 ;
+    while(setbit!=1)
+    {
+        setbit = ans&1 ;
+        
+        pos++;
+        ans=ans>>1 ;
+    }
+  
+    int newxor = 0 ;
+    for(int i = 0 ; i<8 ; i++)
+    {
+        if (arr[i]&(1<<pos-1))
+        {
+           newxor^=arr[i] ;
+        }
+    }
+    cout<<newxor<<endl;
+    cout<< (newxor^temp) <<endl;
+   
+    
+    return 0 ;
+}
